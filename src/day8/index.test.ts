39693f20c28f93c4dff9080213f9d42699173d74
María Simó <holasoymariasimo@gmail.com>
2021-12-15 20:30:37 +0100

    feat(day8): solve part 2

--- src/day8/index.test.ts
+++ src/day8/index.test.ts
@@ -1,10 +1,38 @@
 import { readInputAsArray } from '../utils';
-import { parseInput, countUniqueLengthDigits } from './logic';
+import {
+  parseInput,
+  countUniqueLengthDigits,
+  decodeSignalPatterns,
+  decodeOutputValue,
+  decodeEntriesAndSumOutputValues,
+} from './logic';
 
 const input = readInputAsArray({ dayNumber: 8, isExample: true });
-const outputValueList = parseInput(input)
-  .map((entry) => entry.outputValue)
-  .flat();
+const parsedInput = parseInput(input);
+
+const outputValueList = parsedInput.map((entry) => entry.outputValue).flat();
+
+const signalPatterns =
+  'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab'
+    .split(' ')
+    .map((signal) => [...signal].sort().join(''));
+
+const outputValue = 'cdfeb fcadb cdfeb cdbaf'
+  .split(' ')
+  .map((signal) => [...signal].sort().join(''));
+
+const digits = {
+  '0': 'abcdeg',
+  '1': 'ab',
+  '2': 'acdfg',
+  '3': 'abcdf',
+  '4': 'abef',
+  '5': 'bcdef',
+  '6': 'bcdefg',
+  '7': 'abd',
+  '8': 'abcdefg',
+  '9': 'abcdef',
+};
 
 describe('Day8: Seven Segment Search', () => {
   describe('Generic', () => {
@@ -21,6 +49,23 @@ describe('Day8: Seven Segment Search', () => {
     describe('Count unique length digits', () => {
       test('Should return the number of unique length digits in the output value strings', () => {
         expect(countUniqueLengthDigits(outputValueList)).toBe(26);
+      });
+    });
+  });
+  describe('Part 2', () => {
+    describe('Decode Signal Patterns', () => {
+      test('Should return an object with the correspondency between patterns and digits', () => {
+        expect(decodeSignalPatterns(signalPatterns)).toStrictEqual(digits);
+      });
+    });
+    describe('Decode Output Value', () => {
+      test('Should return correct number value', () => {
+        expect(decodeOutputValue(outputValue, digits)).toBe(5353);
+      });
+    });
+    describe('Decode entries and sum output values', () => {
+      test('Should return correct number value', () => {
+        expect(decodeEntriesAndSumOutputValues(parsedInput)).toBe(61229);
       });
     });
   });
--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,4 +1,17 @@
-import { logResults, readInputAsArray } from "../utils"; const input =
-readInputAsArray(\\{ dayNumber:8\\}); const outputPart1 = 0; const
-outputPart2 = 0; logResults(8, [outputPart1, outputPart2]); export {
-outputPart1, outputPart2 };
+import { logResults, readInputAsArray } from '../utils';
+import {
+  countUniqueLengthDigits,
+  decodeEntriesAndSumOutputValues,
+  parseInput,
+} from './logic';
+
+const input = readInputAsArray({ dayNumber: 8 });
+const parsedInput = parseInput(input);
+
+const outputValueList = parsedInput.map((entry) => entry.outputValue).flat();
+
+const outputPart1 = countUniqueLengthDigits(outputValueList);
+const outputPart2 = decodeEntriesAndSumOutputValues(parsedInput);
+
+logResults(8, [outputPart1, outputPart2]);
+export { outputPart1, outputPart2 };
